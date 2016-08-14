ea1456843c52aae5971b65d567457c63bcf78c49
myapos <myapos@yahoo.com>
2016-08-14 15:53:46 +0300

    Added css in main page

--- app/components/Main.js
+++ app/components/Main.js
@@ -7,19 +7,33 @@ export default class Main extends Component {
  
   render() {
     return (
-      <div>
         <div>
           <h2>Hello from Main Control Panel</h2>
-          <Link to="/">Logout</Link>
-        </div>
-        <Link to="/getallusers">GetAllUsers</Link><br/>
-        <Link to="/adduser">AddUser</Link><br/>
-        <Link to="/deleteuser">SearchOrDeleteUser</Link><br/>
-        <Link to="/updateuser">UpdateUser</Link><br/>
-        <Link to="/importusers">ImportUsers</Link><br/>
-        <Link to="/exportusers">ExportUsers</Link>
-        
+          <div className="container">
+              <div className="row">
+                <div className="col-xs-2"> <Link to="/getallusers">GetAllUsers</Link></div>
+              </div>
+              <div className="row">
+                <div className="col-xs-2"> <Link to="/adduser">AddUser</Link></div>
+              </div>
+              <div className="row"> 
+                <div className="col-xs-2"> <Link to="/deleteuser">SearchOrDeleteUser</Link></div>
+              </div>
+              <div className="row">
+                <div className="col-xs-2"> <Link to="/updateuser">UpdateUser</Link></div>
+              </div>
+              <div className="row">
+                <div className="col-xs-2"> <Link to="/importusers">ImportUsers</Link></div>
+              </div>
+              <div className="row">
+                 <div className="col-xs-2"> <Link to="/exportusers">ExportUsers</Link></div>
+              </div>
+              <div className="row">
+                 <div className="col-xs-2"> <Link to="/">Logout</Link></div>
+              </div>
+          </div>
       </div>
+
     );
   }
 }
